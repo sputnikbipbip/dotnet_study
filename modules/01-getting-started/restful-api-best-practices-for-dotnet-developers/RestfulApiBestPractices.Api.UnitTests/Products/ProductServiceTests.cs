1e6c0a002f4abb8ffb261090a128c647710d21c5
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2026-07-03 14:59:58 +0100

    test: add unit test project for product service and git ignore file

---
+++ modules/01-getting-started/restful-api-best-practices-for-dotnet-developers/RestfulApiBestPractices.Api.UnitTests/Products/ProductServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using AwesomeAssertions;
+using RestfulApiBestPractices.Api.DTOs;
+using RestfulApiBestPractices.Api.Services;
+using Xunit;
+
+namespace RestfulApiBestPractices.Api.UnitTests;
+
+public class ProductServiceTests
+{
+    private static readonly ProductService _productService = new();
+    
+    [Fact]
+    public async Task GetByIdAsync_ReturnsProduct()
+    {
+        //Arrange
+        var product = await _productService.CreateAsync(
+            new CreateProductRequest(
+                "Laptop",
+                "Test product only for testing",
+                99.99m,
+                100,
+                "Test category"
+            ));
+
+        //Act
+        var result = await _productService.GetByIdAsync(product.Id);
+        
+        //Assert
+        result.Should().BeSameAs(product);
+    }
+}
